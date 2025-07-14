71b4e75f82c6de9b62c3748fd67ab12248373627
Ray Cho <skshpapa80@gmail.com>
2025-07-14 23:26:53 +0900

    update Form Design

--- umain.pas
+++ umain.pas
@@ -5,10 +5,20 @@ unit umain;
 interface
 
 uses
-    Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+    Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+    DSPack;
 
 type
+
+    { TfrmMain }
+
     TfrmMain = class(TForm)
+        DSScreen: TDSVideoWindowEx2;
+        DSTrackBar1: TDSTrackBar;
+        FilterGraph1: TFilterGraph;
+        lblCur: TLabel;
+        lblTotal: TLabel;
+        paButton: TPanel;
     private
 
     public
